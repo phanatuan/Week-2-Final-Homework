f66f8bef1d60f08a53424b5b61b28fb0718006f8
Phan Anh Tuan <phananhtuan1011@gmail.com>
2018-11-09 14:33:54 +0700

    add file to a project

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
 //create the Random Number for people to guess
-var randomNumber = Math.floor(Math.random() * 5); 
+var randomNumber = Math.floor(Math.random() * 10 + 1); 
 var message = '';
 var click = 0;
+var pastGuess = [];
 
 function guess() {
 	var userGuess = parseInt(document.getElementById('userGuess').value); 
-	click ++;
-	if (userGuess == randomNumber) { 
-		message = 'Awesome, you guess correctly.'; 
-	else if (userGuess < randomNumber) { 
-		message = 'Your guess is lower than the random Number. Try again';
+	
+	//check if userGuess is null or undefined.If it is, alert, then set the value as 0;
+	if (isNaN(userGuess)){
+		alert ('Please enter a number');
 	} else {
-		message = 'Your guess is higher than the random Number. Try again';
+		pastGuess.push(userGuess);
+		click++;
+		document.getElementById('message').removeAttribute("class"); //remove any existing class 
 	}
-	}
-	//Output:
-	document.getElementById('message').innerHTML = message; 
-	document.getElementById('click').innerHTML = click;
 
+	if(click>9) {
+		message = 'GAME OVER. You have run out of click. Do you want to restart the game?';
+		document.getElementById('guess').disabled = true;
+	} else if (userGuess == randomNumber) { 
+			message = `Awesome, you guess correctly after ${click} times. Game end. Click restart to play again`;
+			document.getElementById('guess').disabled = true;
+			document.getElementById('message').classList.add('bg-success');
+		} 
+		else if (userGuess < randomNumber) { 
+			message = `Your guess is lower than the mystery number. You have ${10-click} guess left. Try again`;
+			document.getElementById('message').classList.add('bg-danger');
+		} else if (userGuess >randomNumber) {
+			message = `Your guess is higher than the mystery number. You have ${10-click} guess left. Try again`;
+			document.getElementById('message').classList.add('bg-danger');
+		}
+	
+	//Display Message and Past Guesses
+	document.getElementById('message').innerHTML = message; 
+	document.getElementById('pastGuess').innerHTML = pastGuess.join(', ');
 }
+	
+//Restart the game
+function reset() {
+	document.getElementById('message').innerHTML = ""; 
+	document.getElementById('pastGuess').innerHTML = "";
+	document.getElementById('guess').disabled = false;
+	document.getElementById('userGuess').value = '';
+	randomNumber = Math.floor(Math.random() * 10 + 1); 
+	click = 0;
+	pastGuess = [];
+}
+	
+	
